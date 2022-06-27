aa66d7a942b22cb8fec2f07bbc242a6da282b325
maxpowernz <helmoreman@gmail.com>
2022-06-27 21:49:57 +1200

    animation

--- src/Modal.js
+++ src/Modal.js
@@ -1,5 +1,13 @@
 import { useEffect, useState } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const fadeIn = keyframes`
+    from {
+        opacity: 0;
+    } to {
+        opacity: 100;
+    }
+`;
 
 const ModalBackground = styled.div`
   position: fixed;
@@ -13,6 +21,7 @@ const ModalBackground = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  animation: ${fadeIn} 1s;
 `;
 
 const ModalBody = styled.div`
